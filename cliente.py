aa1a13b1117f1619f31f168b6921bf5e3a0c0e4b
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-25 15:24:33 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,10 +1,17 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect( ('192.168.1.71',7080) )
 
-mensaje = input('Introduce el mensaje >>  ')
-s.send(bytes(mensaje, "utf-8"))
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
-s.close()
+ciclo = True
+while ciclo:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect( ('192.168.1.71',7080) )
+    mensaje = input('Introduce el mensaje >>  ')
+    print()
+    s.send(bytes(mensaje, "utf-8"))
+    msg = s.recv(1024)
+    print(msg.decode("utf-8"))
+    s.close()
+    if mensaje == "SALIR":
+        ciclo = False
+print()
+print("ADIOS")
