a01bc798028d728ef4c5167fd29d02250e113e44
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-25 22:31:50 -0500

    Update pesa_prueba.py

--- pesa_prueba.py
+++ pesa_prueba.py
@@ -8,6 +8,7 @@ from hx711 import HX711  # import the class HX711
 GPIO.setmode(GPIO.BCM)
 hx = HX711(dout_pin=5, pd_sck_pin=6)
 
+swap_file_name = 'swap_file.swp'
 if os.path.isfile(swap_file_name):
     with open(swap_file_name, 'rb') as swap_file:
         hx = pickle.load(swap_file)
