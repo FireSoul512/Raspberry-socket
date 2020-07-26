b184095198bbb03ffae1c383e6abf54fa2cc9cdd
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-25 23:44:32 -0500

    Update peso.py

--- peso.py
+++ peso.py
@@ -11,15 +11,14 @@ class PESO:
         hx = HX711(dout_pin=5, pd_sck_pin=6)
 
         swap_file_name = 'swap_file.swp'
-        print("Inicio")
         if os.path.isfile(swap_file_name):
             with open(swap_file_name, 'rb') as swap_file:
                 hx = pickle.load(swap_file)
             peso = int(hx.get_weight_mean(20))
             GPIO.cleanup()
-            print("Fin 1")
             return peso
             
         else:
             GPIO.cleanup()
-            return "No hay datos cargados, porfavor ejecutar la app para guardarlos ;v"
+            XD = int(-500)
+            return XD
