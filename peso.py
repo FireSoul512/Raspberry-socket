5c66109014b9c1bf36375b7a71667b19e9fc2a18
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-25 23:01:56 -0500

    Create peso.py

---
+++ peso.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+class PESO:
+    def obtener(self):
+        import pickle
+        import os
+
+        import RPi.GPIO as GPIO  # import GPIO
+        from hx711 import HX711  # import the class HX711
+        GPIO.setmode(GPIO.BCM)
+        hx = HX711(dout_pin=5, pd_sck_pin=6)
+
+        swap_file_name = 'swap_file.swp'
+        if os.path.isfile(swap_file_name):
+            with open(swap_file_name, 'rb') as swap_file:
+                hx = pickle.load(swap_file)
+            
+            peso = int(hx.get_weight_mean(20))
+            GPIO.cleanup()
+            return peso
+            
+        else:
+            print("No hay datos cargados, porfavor ejecutar la app para guardarlos ;v")
+            GPIO.cleanup()
+            return False
