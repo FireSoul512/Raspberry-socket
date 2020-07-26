afbb1b71ef8ab38785a3d1eb7a9a9d0036dfdcf4
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-25 20:56:57 -0500

    Update pesa_prueba.py

--- pesa_prueba.py
+++ pesa_prueba.py
@@ -1,3 +1,17 @@
 import RPi.GPIO as GPIO  # import GPIO
 from hx711 import HX711  # import the class HX711
 
+try:
+    GPIO.setmode(GPIO.BCM)
+    hx = HX711(dout_pin=5, pd_sck_pin=6)
+    err = hx.zero()
+    if err:
+        raise ValueError('Tare is unsuccessful.')
+    reading = float(-957009)
+    value = float(34)
+    ratio = reading / value
+    hx.set_scale_ratio(ratio)
+    print(hx.get_weight_mean(20))
+
+finally:
+    GPIO.cleanup()
