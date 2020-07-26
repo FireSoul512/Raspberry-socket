5cc2eac5ca3afdad32ec0700fbdd0174d170ba23
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-26 13:35:57 -0500

    Update servo.py

--- servo.py
+++ servo.py
@@ -3,6 +3,23 @@
 
 class SERVO:
 
+    def start(self):
+        import RPi.GPIO as GPIO
+        import time
+
+        GPIO.setmode(GPIO.BOARD)
+
+        GPIO.setup(11,GPIO.OUT)
+        servo = GPIO.PWM(11,50)
+
+        servo.start(0)
+        servo.ChangeDutyCycle(2)
+        time.sleep(0.5)
+        servo.ChangeDutyCycle(0)
+
+        servo.stop()
+        GPIO.cleanup()
+
     def mover(self):
 
         import RPi.GPIO as GPIO
