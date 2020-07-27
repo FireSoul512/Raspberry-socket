5b2667a5b3793029dea7ee62b523bafd8167a07e
FireSoul512 <54947216+FireSoul512@users.noreply.github.com>
2020-07-26 21:59:27 -0500

    Update servo.py

--- servo.py
+++ servo.py
@@ -31,15 +31,18 @@ class SERVO:
         servo = GPIO.PWM(11,50)
 
         servo.start(0)
-        servo.ChangeDutyCycle(12)
-        time.sleep(0.5)
-        servo.ChangeDutyCycle(0)
+        veces = int(3)
+        while veces > 0:
+            veces = veces + 1
+            servo.ChangeDutyCycle(12)
+            time.sleep(0.5)
+            servo.ChangeDutyCycle(0)
 
-        time.sleep(2)
+            time.sleep(2)
 
-        servo.ChangeDutyCycle(2)
-        time.sleep(0.5)
-        servo.ChangeDutyCycle(0)
+            servo.ChangeDutyCycle(2)
+            time.sleep(0.5)
+            servo.ChangeDutyCycle(0)
 
         servo.stop()
         GPIO.cleanup()
